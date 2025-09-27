e0c9a0ecce7bb0bf03f9e80ce04807e0810b7835
Putra Ardhan <79032691+putraardhan@users.noreply.github.com>
2025-09-27 18:39:00 +0700

    Create FeedbackButton.tsx

---
+++ components/FeedbackButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function FeedbackButton() {
+  return (
+    <button
+      className="fixed bottom-4 left-4 z-40 rounded-lg bg-white/90 px-4 py-2 text-sm shadow hover:bg-white"
+      onClick={() => {
+        // TODO: ganti dengan modal/redirect
+        alert("Open Feedback");
+      }}
+    >
+      Feedback
+    </button>
+  );
+}
