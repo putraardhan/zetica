ce57930f6bf0cb04596cadac0af3ae6dbdf24d59
Putra Ardhan <79032691+putraardhan@users.noreply.github.com>
2025-09-27 19:21:47 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import \"./globals.css\";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
@@ -27,24 +27,16 @@ export default function RootLayout({
 
           {/* Main content */}
           <div className="flex-1 relative">
-            {/* Connect Wallet button - pojok kanan atas */}
+            {/* Top right connect wallet */}
             <ConnectWalletButton />
 
-            {/* Feedback button - pojok kiri bawah */}
-            <button
-              className="fixed bottom-4 left-4 z-40 rounded-lg bg-white/90 px-4 py-2 text-sm shadow hover:bg-white"
-              onClick={() => {
-                // TODO: ganti dengan fungsi feedback modal/redirect kamu
-                alert("Open Feedback");
-              }}
-            >
-              Feedback
-            </button>
+            {/* Bottom left feedback */}
+            <FeedbackButton />
 
             {/* Page content */}
             <main className="p-6">{children}</main>
 
-            {/* Theme toggle, kalau memang mau tetap di kanan bawah */}
+            {/* Bottom right theme toggle */}
             <div className="fixed bottom-4 right-4">
               <ThemeToggle />
             </div>
