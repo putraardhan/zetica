95588db1195bc94bba4dbbf27949492192af84af
Putra Ardhan <79032691+putraardhan@users.noreply.github.com>
2025-09-27 19:33:07 +0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
-      <h1 className="text-3xl font-bold mb-2">404</h1>
+      <h1 className="text-4xl font-bold mb-2">404</h1>
       <p className="text-neutral-600 mb-6">This page could not be found.</p>
       <button
         onClick={() => router.push("/")}
